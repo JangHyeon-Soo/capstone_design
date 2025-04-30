f7a414107af1c0845bf79d024630e0dbab43a52e
JangHyeon-Soo <jhs513kr@naver.com>
2025-04-30 10:20:41 +0900

    20250430

---
+++ Assets/MonsterController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
